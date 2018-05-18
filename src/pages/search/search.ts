f9a4e0e614ca2f1ddf4fd2d1c6818bd201bfcea1
Khanh Luong <khanhld1910@gmail.com>
2018-05-18 13:03:26 +0700

    updated

---
+++ src/pages/search/search.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-search',
+  templateUrl: 'search.html'
+})
+
+export class SearchPage {
+
+  constructor() {
+
+  }
+}
